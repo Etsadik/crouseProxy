f81b22e3b41b91bf4023de3a3652b0f47da914d8
Elias Tsadik <etsadik@gmail.com>
2020-04-17 14:13:27 -0700

    Add carousel image route

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const app = express();
 const port = 8000;
 
 app.listen(port, () => {
-  console.log(`Colossal Backend is listening on port ${port}`);
+  console.log(`proxy listening on ${port}`);
 });
 
 app.use(express.static(path.join(__dirname, './public')));  
 
 const Reservations = 'http://18.222.48.218';
 const Reviews = 'http://18.220.138.231:3000';
-// const Carousel = 'http://localhost:5000';
+const Carousel = 'http://54.183.250.146:3010';
 
 
 // Reservations
@@ -31,4 +31,10 @@ app.all('/reservations/:restaurantId/:partySize', (req, res) => {
 app.all('/restaurants/:restaurantId/reviews', (req, res) => {
   console.log('redirecting to server 2');
   apiProxy.web(req, res, { target: Reviews });
+});
+
+
+app.all('/api/restaurant/30/carousel', (req, res) => {
+  console.log('redirecting to server 2');
+  apiProxy.web(req, res, { target: Carousel });
 });
